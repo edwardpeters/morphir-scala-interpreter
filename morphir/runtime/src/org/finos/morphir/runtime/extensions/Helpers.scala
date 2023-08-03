f2903a0e5c9f2b789e6f6446cb0956873e5d73ac
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-03 07:29:54 -0700

    Native functiona arities

--- morphir/runtime/src/org/finos/morphir/runtime/extensions/Helpers.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/extensions/Helpers.scala
@@ -6,4 +6,12 @@ object Helpers {
   trait NativeFunction1[T, R] extends NativeFunction with ((T) => R){
     def apply(arg : T) : R
   }
+
+  trait NativeFunction2[T1, T2, R] extends NativeFunction with ((T1, T2) => R) {
+    def apply(arg1: T1, arg2: T2): R
+  }
+
+  trait NativeFunction3[T1, T2, T3, R] extends NativeFunction with ((T) => R) {
+    def apply(arg1: T1, arg2: T2, arg3: T3): R
+  }
 }
