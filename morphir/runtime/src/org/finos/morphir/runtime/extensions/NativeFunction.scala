5b30da7896b54f40800f92dfb4d33995b075904e
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-03 07:37:21 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/extensions/NativeFunction.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/extensions/NativeFunction.scala
@@ -3,11 +3,18 @@ object NativeFunction {
   sealed trait NativeFunction {
     def fqName: FQName
   }
+  object NativeFunction{
+    def apply[T, R](name : FQName, f : (T) => R): Unit ={
+      new NativeFunction1{
+        def fqName: FQName = name
+        def apply(arg: T): R = f(arg)
+      }
+    }
+  }
 
   trait NativeFunction1[T, R] extends NativeFunction with ((T) => R) {
     def apply(arg: T): R
   }
-
   trait NativeFunction2[T1, T2, R] extends NativeFunction with ((T1, T2) => R) {
     def apply(arg1: T1, arg2: T2): R
   }
