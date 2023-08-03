69fad75fa21d4474b1263ef0c45a2bbefce2c483
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-03 07:35:08 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/extensions/Helpers.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/extensions/Helpers.scala
@@ -1,10 +1,3 @@
 object Helpers {
 
-  trait NativeFunction2[T1, T2, R] extends NativeFunction with ((T1, T2) => R) {
-    def apply(arg1: T1, arg2: T2): R
-  }
-
-  trait NativeFunction3[T1, T2, T3, R] extends NativeFunction with ((T1, T2, T3) => R) {
-    def apply(arg1: T1, arg2: T2, arg3: T3): R
-  }
 }
--- morphir/runtime/src/org/finos/morphir/runtime/extensions/NativeFunction.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/extensions/NativeFunction.scala
@@ -7,5 +7,13 @@ object NativeFunction {
   trait NativeFunction1[T, R] extends NativeFunction with ((T) => R) {
     def apply(arg: T): R
   }
+
+  trait NativeFunction2[T1, T2, R] extends NativeFunction with ((T1, T2) => R) {
+    def apply(arg1: T1, arg2: T2): R
+  }
+
+  trait NativeFunction3[T1, T2, T3, R] extends NativeFunction with ((T1, T2, T3) => R) {
+    def apply(arg1: T1, arg2: T2, arg3: T3): R
+  }
   
 }
